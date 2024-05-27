a836b2f6087db61a4511c13ae6b8d72428de90bd
karamakcsc <karamakcsc@hotmail.com>
2024-05-27 17:02:15 +0300

    report

--- masar_assets/custom/user/user.py
+++ masar_assets/custom/user/user.py
@@ -3,7 +3,7 @@ import frappe
 @frappe.whitelist()
 def gen_hash():
     password = frappe.generate_hash(length=16)
-    print_password = frappe.msgprint('Password Generated Successfully:' + str(password) + '\n' + 'Copy the Password and Set it in the Field and Save')
+    print_password = frappe.msgprint('Password Generated Successfully: (' + str(password) + ')\n' + 'Copy the Password and Set it in the Field and Save')
 
     return {
         'password': password,
