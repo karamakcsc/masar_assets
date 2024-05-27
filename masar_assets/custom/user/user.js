ffced0ae97dc289717dac841c4b858e37f6b1719
karamakcsc <karamakcsc@hotmail.com>
2024-05-27 17:28:43 +0300

    User

--- masar_assets/custom/user/user.js
+++ masar_assets/custom/user/user.js
@@ -1,20 +1,23 @@
 frappe.ui.form.on("User", {
     onload: function(frm) {
-        if (!frappe.user.has_role('User Management-Maker')) {
+        if (!frappe.user.has_role('JKB User Management-Maker')) {
             frm.toggle_display("enabled", true);
         } else {
             frm.toggle_display("enabled", false);
         }
+        
         frm.add_custom_button(__("Generate Password"), () => {
             frappe.call({
                 method : 'masar_assets.custom.user.user.gen_hash',
             })
 
         });
+
+        frm.set_df_property('enabled', 'default', 0);
     },
 
     refresh: function(frm) {
-        if (!frappe.user.has_role('User Management-Maker')) {
+        if (!frappe.user.has_role('JKB User Management-Maker')) {
             frm.toggle_display("enabled", true);
         } else {
             frm.toggle_display("enabled", false);
@@ -26,5 +29,13 @@ frappe.ui.form.on("User", {
             })
 
         });
+
+        frm.set_df_property('enabled', 'default', 0);
+
+    },
+    setup: function(frm) {
+
+        frm.set_df_property('enabled', 'default', 0);
+
     }
 });
