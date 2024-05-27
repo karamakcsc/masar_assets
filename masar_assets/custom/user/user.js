43861d0cf95daea715e6470db3cc0ace7d9c2710
karamakcsc <karamakcsc@hotmail.com>
2024-05-27 17:38:09 +0300

    User

--- masar_assets/custom/user/user.js
+++ masar_assets/custom/user/user.js
@@ -1,10 +1,10 @@
 frappe.ui.form.on("User", {
     onload: function(frm) {
-        if (!frappe.user.has_role('JKB User Management-Maker')) {
-            frm.toggle_display("enabled", true);
-        } else {
-            frm.toggle_display("enabled", false);
-        }
+        // if (!frappe.user.has_role('JKB User Management-Maker')) {
+        //     frm.toggle_display("enabled", true);
+        // } else {
+        //     frm.toggle_display("enabled", false);
+        // }
         
         frm.add_custom_button(__("Generate Password"), () => {
             frappe.call({
@@ -13,15 +13,15 @@ frappe.ui.form.on("User", {
 
         });
 
-        frm.set_df_property('enabled', 'default', 0);
+        // frm.set_df_property('enabled', 'default', 0);
     },
 
     refresh: function(frm) {
-        if (!frappe.user.has_role('JKB User Management-Maker')) {
-            frm.toggle_display("enabled", true);
-        } else {
-            frm.toggle_display("enabled", false);
-        }
+        // if (!frappe.user.has_role('JKB User Management-Maker')) {
+        //     frm.toggle_display("enabled", true);
+        // } else {
+        //     frm.toggle_display("enabled", false);
+        // }
 
         frm.add_custom_button(__("Generate Password"), () => {
             frappe.call({
@@ -30,12 +30,25 @@ frappe.ui.form.on("User", {
 
         });
 
-        frm.set_df_property('enabled', 'default', 0);
+        // frm.set_df_property('enabled', 'default', 0);
 
-    },
-    setup: function(frm) {
+    }
+    // setup: function(frm) {
 
-        frm.set_df_property('enabled', 'default', 0);
+    //     frm.set_df_property('enabled', 'default', 0);
 
+    // }
+});
+
+
+
+
+
+frappe.ui.form.on("User", "onload", function(frm) {
+    if (!frappe.user.has_role('System Manager') && !frappe.user.has_role('JKB User Management-Checker')  ) {
+        var df_enabled = frappe.meta.get_docfield("User", "enabled", frm.doc.name);
+        df_enabled.read_only = 0;
+        frm.toggle_display("enabled", false);
+        frm.refresh_fields();
     }
 });
