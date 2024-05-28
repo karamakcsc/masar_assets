26e076b59a602d92ab7eac003603b6c3b81e2837
karamakcsc <karamakcsc@hotmail.com>
2024-05-28 16:09:43 +0300

    update

--- masar_assets/custom/user/user.js
+++ masar_assets/custom/user/user.js
@@ -1,54 +1,50 @@
 frappe.ui.form.on("User", {
     onload: function(frm) {
-        // if (!frappe.user.has_role('JKB User Management-Maker')) {
-        //     frm.toggle_display("enabled", true);
-        // } else {
-        //     frm.toggle_display("enabled", false);
-        // }
-        
         frm.add_custom_button(__("Generate Password"), () => {
             frappe.call({
                 method : 'masar_assets.custom.user.user.gen_hash',
             })
 
         });
-
-        // frm.set_df_property('enabled', 'default', 0);
     },
 
     refresh: function(frm) {
-        // if (!frappe.user.has_role('JKB User Management-Maker')) {
-        //     frm.toggle_display("enabled", true);
-        // } else {
-        //     frm.toggle_display("enabled", false);
-        // }
-
         frm.add_custom_button(__("Generate Password"), () => {
             frappe.call({
                 method : 'masar_assets.custom.user.user.gen_hash',
             })
 
         });
-
-        // frm.set_df_property('enabled', 'default', 0);
-
     }
-    // setup: function(frm) {
-
-    //     frm.set_df_property('enabled', 'default', 0);
-
-    // }
 });
 
 
 
 
 
-frappe.ui.form.on("User", "onload", function(frm) {
+frappe.ui.form.on("User", "refresh", function(frm) {
     if (!frappe.user.has_role('System Manager') && !frappe.user.has_role('JKB User Management-Checker')  ) {
         var df_enabled = frappe.meta.get_docfield("User", "enabled", frm.doc.name);
         df_enabled.read_only = 0;
         frm.toggle_display("enabled", false);
         frm.refresh_fields();
+    }
+});
+frappe.ui.form.on("User", "onload", function(frm) {
+    if (!frappe.user.has_role('System Manager') && !frappe.user.has_role('Workspace Manager')){
+        frm.toggle_display("sb_allow_modules", false);
+        frm.toggle_display("module_profile", false);
+        frm.toggle_display("modules_html", false);
+        frm.toggle_display("block_modules", false);
+        frm.toggle_display("home_settings", false);
+        frm.refresh_fields();
+    }
+});
+
+frappe.ui.form.on("Role", "onload", function(frm) {
+    if (!frappe.user.has_role('System Manager')) {
+        frm.toggle_display("disabled", false)
+        frm.refresh_fields();
+        frm.set_visible("disabled", false);
     }
 });
