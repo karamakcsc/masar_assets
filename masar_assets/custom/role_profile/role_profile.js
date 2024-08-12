13128a40aa4211e922da8e9b0ff36b3d2779dbd3
karamakcsc <karamakcsc@hotmail.com>
2024-08-12 16:05:48 +0300

    update

--- masar_assets/custom/role_profile/role_profile.js
+++ masar_assets/custom/role_profile/role_profile.js
@@ -1,15 +1,19 @@
 frappe.ui.form.on("Role Profile", {
 	refresh: function (frm) {
-		if (!frappe.user.has_role('System Manager') || !frappe.user.has_role('Workspace Manager')) {
-				const roles_to_hide = ["System Manager", "Workspace Manager" , "Script Manager"];
-				frm.roles_editor.wrapper.find(`[data-unit="${roles_to_hide.join('"], [data-unit="')}"]`).hide()
+			if (!frm.roles_editor) {
+				const role_area = $(frm.fields_dict.roles_html.wrapper);
+				frm.roles_editor = new frappe.RoleEditor(role_area, frm);
 			}
+			frm.roles_editor.show();
 
-	}, 
-	onload: function(frm){
-		if (!frappe.user.has_role('System Manager') || !frappe.user.has_role('Workspace Manager')) {
-			const roles_to_hide = ["System Manager", "Workspace Manager" , "Script Manager"];
-			frm.roles_editor.wrapper.find(`[data-unit="${roles_to_hide.join('"], [data-unit="')}"]`).hide()
-		}
 	}
+});
+
+frappe.ui.form.on('Role Profile', {
+    refresh: function(frm) {
+		if (!frappe.user.has_role("System Manager")) {
+        $('input[data-unit="System Manager"]').closest('.checkbox').hide();
+        $('input[data-unit="Script Manager"]').closest('.checkbox').hide();
+    }
+}
 });
