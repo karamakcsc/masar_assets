a968956ad6b4dfb86ae55db522f8a253c4e1a2b0
karamakcsc <karamakcsc@hotmail.com>
2024-08-13 17:00:38 +0300

    update

--- masar_assets/custom/department/department.js
+++ masar_assets/custom/department/department.js
@@ -2,14 +2,17 @@ frappe.ui.form.on('Department', {
     onload: function(frm) {
         set_doctype_read_only(frm);
         // frm.toggle_display("company", false);
+        frm.set_df_property('company', 'read_only', 1);
     },
     refresh: function(frm) {
         set_doctype_read_only(frm);
         // frm.toggle_display("company", false);
+        frm.set_df_property('company', 'read_only', 1);
     },
     setup: function(frm) {
         set_doctype_read_only(frm);
         // frm.toggle_display("company", false);
+        frm.set_df_property('company', 'read_only', 1);
     }
 });
 
